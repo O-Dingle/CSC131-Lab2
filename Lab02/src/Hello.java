433565a40f090cd2ad5dd42a13bb501efe9b263d
chrubble <47755013+notchrubble@users.noreply.github.com>
2023-10-29 15:35:18 -0700

    Update line 9 with Zach's information

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("301049686 - This line was added by Oanh Dingle.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219598328 - This line was added by Zachary Attiya (https://github.com/notchrubble).");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
