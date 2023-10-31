a9dbfab9881b37d1378bc1fbb3d501c6da4c6df5
Nicholas <nicholaseovva@csus.edu>
2023-10-30 21:00:42 -0700

    lab002 for csc131

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("219598328 - This line was added by Zachary Attiya (https://github.com/notchrubble).");
 		System.out.println("218934002 - This line was added by Tyrice Woods.");
 		System.out.println("303342416 - This line was added by Raz Ibrahimi.");
-		System.out.println("303345211 - This line was added by Nicholas Eovva.");
+		System.out.println("303345211 - This line was added by Nicholas Eovva");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
